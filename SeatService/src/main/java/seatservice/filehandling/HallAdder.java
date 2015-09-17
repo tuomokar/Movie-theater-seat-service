a1ed427b2a8e9643cc1212bd190036e319c10e49
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-09-17 22:02:30 +0300

    added a catch for a possible number format exception

--- SeatService/src/main/java/seatservice/filehandling/HallAdder.java
+++ SeatService/src/main/java/seatservice/filehandling/HallAdder.java
@@ -14,52 +14,60 @@ public class HallAdder {
 
     /**
      * Sets name of the text file containing the information on the halls
+     *
      * @param filePath
      */
     public HallAdder(String filePath) {
         this.filePath = filePath;
     }
-      
+
     /**
      * Writes information about a new hall to the text file. In order for the
-     * file writing to succeed, the parameters must be valid. The hall's name 
-     * must not be null or empty and the rows and the amount of seats in a row 
+     * file writing to succeed, the parameters must be valid. The hall's name
+     * must not be null or empty and the rows and the amount of seats in a row
      * must be more than zero.
+     *
      * @param hallName
      * @param rows
      * @param seatsInARow
      * @return whether or not the writing to the file succeeded
-     * @throws IOException 
      */
-    public boolean createNewHall(String hallName, int rows, int seatsInARow) throws IOException {
+    public boolean createNewHall(String hallName, int rows, int seatsInARow) {
         if (!checkParameters(hallName, rows, seatsInARow)) {
             return false;
         }
 
         Hall hall = new Hall(hallName, rows, seatsInARow);
-        FileWriter writer = new FileWriter(filePath, true);
-        
-        writer.write(hall.toString());
-        writer.write(";\n");
-        writer.close();
-        
+
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+
+            writer.write(hall.toString());
+            writer.write(";\n");
+            writer.close();
+        } catch (IOException exc) {
+            System.out.println("writing to the file was unsuccessful");
+        }
+
         return true;
     }
-    
+
     private boolean checkParameters(String name, int rows, int seatsInARow) {
         return name != null && !name.isEmpty() && rows > 0 && seatsInARow > 0;
     }
-    
+
     /**
      * Changes the current file path to a new one that is given as the parameter
-     * @param newPath 
+     *
+     * @param newPath
      */
     public void changeFilePath(String newPath) {
         filePath = newPath;
     }
-    
+
     /**
      * Returns the current file path of the file
+     *
      * @return file
      */
     public String getFilePath() {
