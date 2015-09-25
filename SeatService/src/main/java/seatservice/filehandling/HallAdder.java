a2dac5be8f5b0e6076d1bcfe2f7ef27fd294685e
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-09-25 10:40:11 +0300

    refactored slightly and updated javadoc

--- SeatService/src/main/java/seatservice/filehandling/HallAdder.java
+++ SeatService/src/main/java/seatservice/filehandling/HallAdder.java
@@ -13,7 +13,7 @@ import seatservice.domain.Halls;
 
 /**
  * This class is responsible for adding new halls to the movie theater's
- * database (which is a text file)
+ * database (which is an xml file)
  */
 public class HallAdder {
 
@@ -21,7 +21,8 @@ public class HallAdder {
     private Halls halls;
 
     /**
-     * Sets name of the text file containing the information on the halls
+     * Sets name of the xml file containing the information on the halls and
+     * also sets the 'halls' instance variable to the one given as the parameter
      *
      * @param filePath
      * @param halls
@@ -32,48 +33,46 @@ public class HallAdder {
     }
 
     /**
-     * Writes information about a new hall to the text file. In order for the
-     * file writing to succeed, the parameters must be valid. The hall's name
-     * must not be null or empty and the rows and the amount of seats in a row
-     * must be more than zero.
+     * Writes information about a new hall to the xml file by using JAXB. First
+     * a new hall is created from the parameters given and it's added to the
+     * list of current halls. Then all the halls are written into the xml file.
+     * In case there's an error, the method returns false, but otherwise the
+     * method returns true.
      *
      * @param hallName
      * @param rows
      * @param seatsInARow
      * @return whether or not the writing to the file succeeded
      */
-    public boolean createNewHall(String hallName, int rows, int seatsInARow) throws JAXBException {
-//        try {
-            Hall hall = new Hall(hallName, rows, seatsInARow);
-            halls.addHall(hall);
+    public boolean createNewHall(String hallName, int rows, int seatsInARow) {
+        Hall hall = new Hall(hallName, rows, seatsInARow);
+        halls.addHall(hall);
+        boolean success = writeAllTheHallsToTheFile();
+        return success;
+        
+    }
 
+    /**
+     * Writes all the halls currently in the list of the halls into the
+     * xml file by using JAXB. Returns false if an error is encountered during
+     * the marshalling and otherwise returns true.
+     *
+     * @param halls
+     * @return
+     * @throws JAXBException
+     */
+    public boolean writeAllTheHallsToTheFile() {
+        try {
             JAXBContext jaxbContext = JAXBContext.newInstance(Halls.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
             jaxbMarshaller.marshal(halls, new File(filePath));
-//        } catch (JAXBException exc) {
-//            throw new JAXBException("didn't succeed");
-//            System.out.println("didn't succeed");
-//            return false;
-//        }
+        } catch (JAXBException exc) {
+            return false;
+        }
         return true;
-    }
-    
-    public boolean writeMultipleHallsAtOnce(Halls halls) throws JAXBException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(Halls.class);
-            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-
-            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            jaxbMarshaller.marshal(halls, new File(filePath));
-            
-            return true;
-    }
-
-    private boolean checkParameters(String name, int rows, int seatsInARow) {
-        return name != null && !name.isEmpty() && rows > 0 && seatsInARow > 0;
     }
 
     /**
@@ -85,11 +84,6 @@ public class HallAdder {
         filePath = newPath;
     }
 
-    /**
-     * Returns the current file path of the file
-     *
-     * @return file
-     */
     public String getFilePath() {
         return filePath;
     }
