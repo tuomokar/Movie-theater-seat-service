c6a3f4621b5aaf37c805c18ebe868e831ed4ca25
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-10-02 21:06:07 +0300

    created action listener for seats

---
+++ SeatService/src/main/java/seatservice/userinterface/customerServing/ClickListener.java
@@ -0,0 +1,21 @@
+
+package seatservice.userinterface.customerserving;
+
+import seatservice.domain.Seat;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClickListener implements ActionListener {
+         
+    @Override
+    public void actionPerformed(ActionEvent event) {       
+        Seat seat = (Seat) event.getSource();
+        
+        if (seat.isAvailable()) {
+            seat.setToUnavailable();
+        } else {
+            seat.setToAvailable();
+        }
+    }
+}
