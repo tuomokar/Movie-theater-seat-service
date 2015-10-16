9a5515dd745f4200252381a9a56e55c890279fdb
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-10-16 11:28:14 +0300

    updated to GUI Builder

--- SeatService/src/main/java/seatservice/Main.java
+++ SeatService/src/main/java/seatservice/Main.java
@@ -1,27 +1,22 @@
-
 package seatservice;
 
-import java.io.IOException;
-import seatservice.filehandling.HallParser;
-import seatservice.userinterface.EmployeeUI;
-import seatservice.userinterface.UserInterface;
 import javax.swing.SwingUtilities;
+import seatservice.userinterface.UserInterface;
 
 /**
  * This class functions as the main class.
  */
 public class Main {
-    
+
     /**
      * Creates the user interface and starts it.
+     *
      * @param args
      */
     public static void main(String[] args) {
-//        EmployeeUI userInterface = new EmployeeUI();
-//        userInterface.start();
-        
+
         UserInterface userInterface = new UserInterface();
         SwingUtilities.invokeLater(userInterface);
-                            
+
     }
 }
