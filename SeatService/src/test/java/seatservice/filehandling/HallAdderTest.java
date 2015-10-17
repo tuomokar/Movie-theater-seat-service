f7e7bdeb5336de6f0f39df34720f7dc8058bb639
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-10-18 00:53:00 +0300

    added a test

--- SeatService/src/test/java/seatservice/filehandling/HallAdderTest.java
+++ SeatService/src/test/java/seatservice/filehandling/HallAdderTest.java
@@ -8,7 +8,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
+import seatservice.domain.Hall;
 import seatservice.domain.Halls;
+import java.util.UUID;
 
 
 public class HallAdderTest {
@@ -40,15 +42,31 @@ public class HallAdderTest {
     @Test
     public void fileWritingIsSuccessful() {
         assertTrue(hallAdder.createNewHall("testHall", 2, 3));
+        assertTrue(hallAdder.getHalls().getHalls().size() == 1);
+        assertTrue(hallAdder.getHalls().contains(new Hall("testHall", 2, 3)));
+    }
+    
+    @Test
+    public void writingMultipleHallsIsSuccessful() {
+        Object[][] info = infoOnMultipleTestHalls();
+        
+        for (int i = 0; i < info.length; i++) {
+            String name = (String) info[i][0];
+            Integer rows = (Integer) info[i][1];
+            Integer seats = (Integer) info[i][2];
+            assertTrue(hallAdder.createNewHall(name, rows, seats));
+            assertTrue(hallAdder.getHalls().getHalls().size() == (i + 1));
+            assertTrue(hallAdder.getHalls().contains(new Hall(name, rows, seats)));
+        }
     }
 
     private Object[][] infoOnMultipleTestHalls() {
         Object[][] info = new Object[5][3];
-        info[0][0] = "testHall1";
-        info[1][0] = "testHall2";
-        info[2][0] = "testHall3";
-        info[3][0] = "testHall4";
-        info[4][0] = "testHall5";
+        info[0][0] = getRandomName();
+        info[1][0] = getRandomName();
+        info[2][0] = getRandomName();
+        info[3][0] = getRandomName();
+        info[4][0] = getRandomName();
 
         info[0][1] = getRandomNumber();
         info[1][1] = getRandomNumber();
@@ -64,8 +82,12 @@ public class HallAdderTest {
 
         return info;
     }
+    
+    private String getRandomName() {
+        return UUID.randomUUID().toString().substring(1, 10);
+    }
 
-    private int getRandomNumber() {
+    private Integer getRandomNumber() {
         return new Random().nextInt(10) + 1;
     }
 
