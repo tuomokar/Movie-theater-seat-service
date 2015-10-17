2a337d11783355aae26dee97ca8efe982ba6028e
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-10-18 01:34:34 +0300

    added variable for removing

--- SeatService/src/main/java/seatservice/userinterface/command/Command.java
+++ SeatService/src/main/java/seatservice/userinterface/command/Command.java
@@ -3,12 +3,14 @@ package seatservice.userinterface.command;
 
 /**
  * This is a simple enum class, intended for use to determine the purpose of
- * use for a method in the InputErrorHandler class.
+ * use for a method in the InputErrorHandler class and another one in the 
+ * UserInterface class.
  * 
  * @author Tuomo Oila
  */
 public enum Command {
     
     UPDATE,
-    ADD;
+    ADD,
+    REMOVE;
 }
