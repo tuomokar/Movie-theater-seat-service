94661be40f9a14bca9a226fc23db79652924ddc8
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-10-18 12:39:22 +0300

    updated javadoc

--- SeatService/src/main/java/seatservice/userinterface/command/Command.java
+++ SeatService/src/main/java/seatservice/userinterface/command/Command.java
@@ -3,8 +3,10 @@ package seatservice.userinterface.command;
 
 /**
  * This is a simple enum class, intended for use to determine the purpose of
- * use for a method in the InputErrorHandler class and a couple of methods in
- * the UILogicHandler class
+ * use for a method in the InputErrorHandler class, couple of methods in the
+ * InputErrorHandler class and a couple of methods in  the UILogicHandler class.
+ * By using these, unnecessary similar code can be reduced as those methods do
+ * largely the same things with small variations.
  * 
  * @author Tuomo Oila
  */
@@ -13,5 +15,6 @@ public enum Command {
     UPDATE,
     ADD,
     REMOVE,
-    RESET;
+    RESET,
+    SHOW;
 }
